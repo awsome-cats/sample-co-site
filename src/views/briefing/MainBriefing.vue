50d8054ad85ed24e1df2ef6472e47a9d08e1b852
awesome-cats <cattwin82@gmail.com>
2020-02-18 18:37:13 +0900

    change briefing page

--- src/views/briefing/MainBriefing.vue
+++ src/views/briefing/MainBriefing.vue
@@ -1,5 +1,5 @@
 <template>
-   <div>
+   <section class="briefing">
       <div class="briefing-page-head">
          <div class="briefing-title">
             <div>
@@ -14,6 +14,8 @@
          </div>
          <div class="briefing-img"></div>
       </div>
+       <h2 class="briefing-flow-image">体験会の流れ</h2>
+      
       
       <div class="briefing-page-flow">
            <div class="briefing-img-section" 
@@ -31,11 +33,14 @@
             </div>
            </div>
       </div>
-      <h2 style="text-align:center; margin:35px;">\ 体験会の申し込み /</h2>
+      
+      <h2 class="briefing-contact">\ 体験会の申し込み /</h2>
       <h3 class="text-center">入力が終えるまで10秒</h3>
       <p class="text-center">プログラミングの世界へ一歩を踏み出す</p>
+     
      <testForm/>
-   </div>
+   
+   </section>
 </template>
 
 <script>
@@ -55,6 +60,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+   .briefing {
    .briefing-page-head {
       width:100%;
       height:auto;
@@ -70,8 +76,7 @@ export default {
             color:#fff;
             font-size:2rem;
             font-weight:600;
-            
-            text-align: center;
+            text-align: start;
             margin-top:50px;
             padding-bottom:20px;
            
@@ -84,6 +89,7 @@ export default {
             margin-left:20px;
             font-weight: 600;
             width:80%;
+            text-align: start;
          }
          p {
             font-size:2rem;
@@ -100,6 +106,13 @@ export default {
          background-position:center;
          margin:10px;
       }
+
+   }
+   .briefing-flow-image {
+      font-size:1.5rem;
+      text-align: center;
+      font-weight: 500;
+      padding-top:30px;
 
    }
    .briefing-page-flow {
@@ -135,8 +148,15 @@ export default {
          }
       }
    }
+   h2.briefing-contact {
+      color:rgb(223, 71, 71);
+      font-size:2rem;
+      margin:30px 0px;
+      text-align: center;
+      font-weight: 600;
+   }
 
-
+}
 // Media Query 768px
 @media screen and (max-width:768px) {
    .briefing-page-head {
@@ -180,45 +200,88 @@ export default {
 //Media Query 414px 
 
 @media screen and (max-width:414px) {
-   .briefing-page-head {
-      flex-direction: column;
-      margin-top:50px;
-      margin-bottom:30px;
-      background:rgb(243, 243, 243);
-      .briefing-title {
+   .briefing {
+      padding:10px 10px;
+      .briefing-page-head {
+         flex-direction: column;
+         margin-top:50px;
+         margin-bottom:30px;
+         background:rgb(243, 243, 243);
+         .briefing-title {
+            width:100%;
+            height:100%;
+            margin:0;
+            h2 {
+               font-size:1.6rem;
+               margin-top:0;
+               padding-bottom:0;
+               text-align: start;
+               margin-bottom:20px;
+            }
+            div p:first-of-type {
+               font-size: 1rem;
+               margin-left:10px;
+               margin-top:0;
+               font-weight:500;
+               text-align: start;
+            }
+             
+         }
+         .briefing-img {
+            width:100%;
+            height:auto;
+            height:200px;
+            margin:0;
+         }
+      }
+      .briefing-page-flow {
+         display:grid;
+         grid-template-rows: 1fr;
+         grid-auto-columns: 1fr;
          width:100%;
-         h2 {
-            font-size:1rem;
-            margin:10px;
+         justify-content: center;
+
+         .briefing-img-section {
+            height:auto;
+            margin:15px 0px;
+            .briefing-img {
+               grid-column: 1fr;
+               grid-row: 1fr;
+               width:100%;
+               height:auto;
+               .title {
+                  height:100%;
+                  p:first-of-type {
+                     padding-top:5px;
+                     font-size: 1rem;
+                     font-weight: 600;
+                  
+                  }
+                  p:last-of-type {
+                     font-size: 1rem;
+                     font-weight: 530;
+                     padding-top:0px;
+                     padding-bottom:0;
+                  }
+               }
+            }
+            h2 {
+               font-size: 1.3rem;
+            }
+            h3 {
+
+            }
+            p {
+
+            }
          }
-         p:first-of-type {
-            font-size: 1.3rem;
-            margin-left:10px;
-         }
-         p:last-of-type {
-            font-size: 1.5rem;
-            font-weight: 400;
-            padding:0;
-            margin-left:10px;
-         }
+         
       }
    }
-   .briefing-page-flow 
-   .briefing-img-section
-   .briefing-img {
-      width:100%;
-      height:auto;
-      .title {
-         p:first-of-type {
-            font-size: 1rem;
-            font-weight: 600;
-         }
-         p:last-of-type {
-            font-size: .7rem;
-            font-weight: 530;
-         }
-      }
-   }
+   
+   
+
+
 }
 
 
